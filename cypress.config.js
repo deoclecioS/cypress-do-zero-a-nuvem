f4d8af9451a44f7b8e9cdd5a96fb03779c136f59
unknown <deoclecio@hotmail.com>
2025-04-07 14:53:01 -0300

    Configuracao do cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   watchForFileChanges: false,
   video: false,
-  
   viewportHeight: 880,
   viewportWidth: 1280,
+  projectId: "vkuy7c",
   e2e: {
   },
 });

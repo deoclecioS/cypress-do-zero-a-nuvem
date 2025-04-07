1e7d7285ecdc7d08350a67ae14313f30c91e6fa7
unknown <deoclecio@hotmail.com>
2025-04-07 12:30:24 -0300

    corrigido o exemplo de teste quebrando

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,7 +6,7 @@ describe('Suíte de Testes', () => {
   })
 
   it('Verificar o título da aplicação', () => {
-    cy.title().should('eq', 'Central de Atendimento ao Cliente T')
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
   })
 
   it('Preencher campos do Formulario', () => {
